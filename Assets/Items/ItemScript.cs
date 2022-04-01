906beef9f74eedf70549366f18005037eb9a0af0
Warp <warpaboi@gmail.com>
2022-04-01 13:32:15 +0200

    Items
    Scripts
    Textures
    First level

---
+++ Assets/Items/ItemScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour
+{
+    public enum ItemList
+    {
+        Empty,
+        Circle,
+        Hexagon,
+        Diamond
+    }
+    int RnItem()
+    {
+        return Random.Range(1, 4);
+    }
+    void Awake()
+    {
+        var i = RnItem();
+        gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(((ItemList)i).ToString());
+    }
+}
